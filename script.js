0cc81a9350b88096b7775f4a11b9dff042a3c2ea
Faiz <76619765+FaizNajib16@users.noreply.github.com>
2024-08-02 11:04:05 +0800

    Refactor code
    The code is refactored so that the value will be case sensitive. The
    user input will takes only rock, paper or scissor. The value of input
    is set to lowercase in the getHumanChoice function rather than in the
    comparison in playRound function.

--- script.js
+++ script.js
@@ -2,39 +2,49 @@ function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3 + 1);
     
     if(choice == 1){
-        return "Rock";
+        return "rock";
     }
     else if(choice == 2){
-        return "Paper";
+        return "paper";
     }
     else{
-        return "Scissor";
+        return "scissor";
     }
 }
 
 function getHumanChoice(){
-    return choice = prompt("Rock, Paper or Scissor?\n 1 - Rock\n 2 - Paper\n 3 - Scissor\n Input the number please." );
+    let choice = prompt("Rock, Paper or Scissor?\n" ).toLowerCase();
+    
+    while(!(choice == "rock" || choice == "paper" || choice == "scissor")){
+        choice = prompt("ERROR! Please provide a valid input.\n So...Rock, Paper or Scissor?\n" ).toLowerCase();
+    }
+
+    return choice;
 }
 
 function playGame(){
     let humanScore = 0;
     let computerScore = 0;
+    let tieScore = 0;
 
     function playRound(humanChoice, computerChoice) {
         // your code here!
-        if(humanChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissor"){
+        console.log(humanChoice);
+        console.log(computerChoice);
+        if(humanChoice == "rock" && computerChoice == "scissor"){
             ++humanScore;
             return "Human Wins!";
         }
-        else if(humanChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock"){
+        else if(humanChoice == "paper" && computerChoice == "rock"){
             ++humanScore;
             return "Human Wins!";
         }
-        else if(humanChoice.toLowerCase() == "scissor" && computerChoice.toLowerCase() == "paper"){
+        else if(humanChoice == "scissor" && computerChoice == "paper"){
             ++humanScore;
             return "Human Wins!";
         }
-        else if(humanChoice.toLowerCase() == computerChoice.toLowerCase()){
+        else if(humanChoice == computerChoice){
+            ++tieScore;
             return "Tie!";
         }
         else{
@@ -44,10 +54,12 @@ function playGame(){
     }
 
     for(let i = 0; i < 5; ++i){
-        playRound(getHumanChoice(),getComputerChoice());
-        console.log("Human: " + humanScore);
-        console.log("computer: " + computerScore);
+        console.log(playRound(getHumanChoice(),getComputerChoice()));
     }
+
+    console.log("Human: " + humanScore);
+    console.log("computer: " + computerScore);
+    console.log("Tie: " + tieScore);
 
     if(humanScore > computerScore){
         return "Human Wins the game!"
